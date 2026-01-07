55920a0d3c8b468968fe0ed4ce920e7ec8d8cfdd
Madssb44 <you@example.com>
2026-01-07 12:18:00 +0100

    more pwm stuff

---
+++ 1. Semester/Subjects/Electronics/Exam prep/pwm.py
@@ -0,0 +1,35 @@
+from machine import Pin, PWM
+from time import sleep_ms
+
+ledrL= Pin(0, Pin.OUT)
+ledgL=Pin(2, Pin.OUT)
+ledyL=Pin(1, Pin.OUT)
+
+
+ledrR = Pin(3, Pin.OUT)
+ledyR = Pin(4, Pin.OUT)
+ledgR = Pin(5, Pin.OUT)
+
+
+
+gR = PWM(ledgR)
+yR = PWM(ledyR)
+rR = PWM(ledrR)
+gL = PWM(ledgL)
+yL = PWM(ledyL)
+rL = PWM(ledrL)
+
+
+
+
+
+
+while True:
+    gR.freq(800)
+    gR.duty_u16(2**16)
+    sleep_ms(250)
+    gR.duty_u16(0)
+    sleep_ms(250)
+
+
+
