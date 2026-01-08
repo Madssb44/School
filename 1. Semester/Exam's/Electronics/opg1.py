83a0df42c790512fdef15f8e7c35853aa8a57cf7
Madssb44 <you@example.com>
2026-01-08 09:41:18 +0100

    exam pwm

--- 1. Semester/Exam's/Electronics/opg1.py
+++ 1. Semester/Exam's/Electronics/opg1.py
@@ -14,25 +14,63 @@ ledgR = Pin(5, Pin.OUT)
 
 
 gR = PWM(ledgR)
-yR = PWM(ledyR)
-rR = PWM(ledrR)
-gL = PWM(ledgL)
-yL = PWM(ledyL)
-rL = PWM(ledrL)
+#yR = PWM(ledyR)
+#rR = PWM(ledrR)
+#gL = PWM(ledgL)
+#yL = PWM(ledyL)
+#rL = PWM(ledrL)
 
 
-
-
-
+def setThings():
+    while True:
+        hz = input("Enter a frequenzy from 0 - 20\n")
+        power = input("Enter how bright you want the light to be from 0% to 100%\n")
+        if hz.isdigit():
+            if power.isdigit():
+                break
+            else:
+                setThings()
+        else:
+            setThings()
+    if int(hz) == 0:
+        print("Invalid input not in range")
+        setThings()
+    elif int(hz) > 20:
+        print("Invalid input too high")
+        setThings()
+    elif int(power) < 0:
+        print("Invalid input too low")
+        setThings()
+    elif int(power) > 100:
+        print("Invalid input too high")
+        setThings() 
+    elif not hz.isdigit():
+        setThings()
+    elif not power.isdigit():
+        setThings()
+    
+    sleeptimer = 1000 / int(hz)
+    duty = int(power) / 100 * 65535 
+    
+    print(int(duty))
+    print(sleeptimer) 
+    gR.freq(800)
+    while True:
+        gR.duty_u16(int(duty))
+        sleep_ms(int(sleeptimer))
+        gR.duty_u16(1)
+        sleep_ms(int(sleeptimer))
 
 while True:
-    gR.freq(800)
-    gL.freq(800)
-    gL.duty_u16(0)
-    gR.duty_u16(2**16)
-    sleep_ms(500)
-    gR.duty_u16(0)
-    gL.duty_u16(2**16)
-    sleep_ms(500)
+    setThings()
+#while True:
+#    gR.freq(800)
+#    gL.freq(800)
+#    gL.duty_u16(0)
+#    gR.duty_u16(2**16)
+#    sleep_ms(500)
+#    gR.duty_u16(0)
+#    gL.duty_u16(2**16)
+#    sleep_ms(500)
 
 
