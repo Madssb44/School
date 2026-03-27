2db7566022daadc184533483e39f4fc35e2cb5d4
Madssb44 <you@example.com>
2026-03-27 07:39:49 +0100

    more flask

--- 2. Semester/Programming/mar-26-2026/flask_test.py
+++ 2. Semester/Programming/mar-26-2026/flask_test.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request 
+from flask import Flask, render_template, url_for, request, redirect  
 
 app = Flask(__name__, static_url_path="/static")
 #name = "Mads"
