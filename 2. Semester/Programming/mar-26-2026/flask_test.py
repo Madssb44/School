da9538184d1f6f23cc8bd783c2b85518a59e693c
Madssb44 <you@example.com>
2026-03-27 12:38:03 +0100

    more on flask, asl well as some encryption things

--- 2. Semester/Programming/mar-26-2026/flask_test.py
+++ 2. Semester/Programming/mar-26-2026/flask_test.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template, url_for, request, redirect  
-
+from hashlib import sha256
 app = Flask(__name__, static_url_path="/static")
 #name = "Mads"
 thing_list = ["And","Need","A","Life"]
 genres = {"rock":["rock","metal","deathcore"], "pop": ["pop","j-pop","k-pop"], "folk": ["irish","celtic","danish"]}
 name = ""
 age = ""
+#user = ""
+#passwd = ""
+aduser = sha256("admin".encode()).hexdigest()
+adpass = sha256("admin".encode()).hexdigest()
+accounts = {"username":[aduser], "password":[adpass], "id":[0]}
+
+def encrypt(user, passwd):
+#    user = user.decode()
+#    passwd = passwd.decode()
+    username = sha256(user.encode()).hexdigest()
+    password = sha256(passwd.encode()).hexdigest()
+    return username, password 
+
+
+
 @app.route("/", methods=['POST','GET'])
 def hello_world():
     global name, age 
@@ -19,20 +34,51 @@ def hello_world():
     <input type='submit'>
     </form>"""
 
+
 @app.route("/old")
 def old():
     return render_template("are_you_old.html", old=int(age))
+
 
 @app.route("/music")
 def music():
     return render_template("music.html", genres=genres)
 
+
 @app.route("/login", methods=['POST','GET'])
 def login_page():
+    if request.method == 'POST':        
+        user = request.form.get('username')        
+        if sha256(user.encode()).hexdigest() in accounts['username']:
+            index = accounts["username"].index(sha256(user.encode()).hexdigest())
+            passwd = request.form.get('password')
+            if sha256(passwd.encode()).hexdigest() == accounts['password'][index]:
+                return render_template('dashboard.html', user=user)
+            else:
+                return render_template("login.html")
+        else:
+            return render_template("login.html")
     return render_template("login.html")
+
 
 @app.route("/create", methods=['POST','GET'])
 def create_account_page():
+    if request.method == 'POST':
+        user = request.form.get('username')
+        passwd = request.form.get('password')
+        if user in accounts["username"]:
+            pass 
+        else:
+            encuser, encpasswd = encrypt(user, passwd)
+            accounts['username'].append(encuser)
+            accounts['password'].append(encpasswd)
+            return render_template("/dashboard", user=user)
     return render_template("create_account.html")
+
+
+@app.route("/dashboard", methods=["POST","GET"])
+def dashboard():
+    return render_template("dashboard.html", user="not logged in yet")
+
 
 app.run(port=5000)
